120b300207a746503e47a8a4eb02925bd629930e
Juhaszky <juhasz.krisztian2000@gmail.com>
2023-12-22 08:40:39 +0100

    Comparsion directive optimized

--- frontend/coffee-scraper/src/app/comparsion.directive.ts
+++ frontend/coffee-scraper/src/app/comparsion.directive.ts
@@ -1,50 +1,58 @@
 import { Directive, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
 import { Product } from './shared/models/product.model';
 import { TableService } from './home/data-access/table.service';
-import { BehaviorSubject } from 'rxjs';
 
 @Directive({
-  selector: '[appComparsion]',
+  selector: '[productComparsion]',
   standalone: true,
 })
-export class ComparsionDirective implements OnDestroy, OnInit {
-  private readonly LOWERED_COLOR = 'green';
-  private readonly ALERT_COLOR = '#D0342C';
+export class ComparsionDirective implements OnDestroy {
+  private readonly LOWERED_COLOR = 'rgb(46, 140, 14, 0.5)';
+  private readonly ALERT_COLOR = 'rgb(208, 52, 44, 0.5)';
   @Input() product!: Product;
   @Input() products: Product[] = [];
 
-  constructor(private el: ElementRef, private tableService: TableService) {}
-  ngOnDestroy(): void {
-    this.tableService.test.unsubscribe();
+  constructor(private el: ElementRef, private tableService: TableService) {
+    this.tableService.doComparsion.subscribe((doCompare) =>
+      doCompare ? this.compareProducts() : false
+    );
   }
-  ngOnInit(): void {
-    this.tableService.test
-      .asObservable()
-      .subscribe((res) => (res ? this.compareProducts() : ''));
+  ngOnDestroy(): void {
+    this.tableService.doComparsion.unsubscribe();
   }
 
-  compareProducts() {
+  private compareProducts() {
     const currentData = this.product;
     const fetchedData = this.findProductByItemNumber(currentData.itemNumber);
-    if (currentData && fetchedData) {
-      if (
-        this.isPriceLowered(currentData.currentPrice, fetchedData.currentPrice)
-      ) {
-        this.el.nativeElement.style.backgroundColor = this.LOWERED_COLOR;
-      } else {
-        this.el.nativeElement.style.backgroundColor = this.ALERT_COLOR;
-      }
-      this.updatePrice(currentData, fetchedData);
+
+    if (!currentData || !fetchedData) {
+      return;
     }
+
+    const isPriceLowered = this.isPriceLowered(
+      currentData.currentPrice,
+      fetchedData.currentPrice
+    );
+    this.el.nativeElement.style.backgroundColor = isPriceLowered
+      ? this.LOWERED_COLOR
+      : this.ALERT_COLOR;
+
+    this.updatePrice(currentData, fetchedData);
   }
+
   private updatePrice(data: Product, fetchedData: Product) {
     data.currentPrice = fetchedData.currentPrice;
     return data;
   }
+
   private isPriceLowered(oldPrice: string, currentPrice: string): boolean {
     return oldPrice > currentPrice;
   }
-  private findProductByItemNumber(itemNumber: string): Product | null {
-    return this.products.find((p) => p.itemNumber === itemNumber) || null;
+
+  private findProductByItemNumber(itemNumber: string): Product | undefined {
+    let product = undefined;
+    product = this.products.find((p) => p.itemNumber === itemNumber);
+    return product;
   }
+  
 }
--- frontend/coffee-scraper/src/app/home/data-access/table.service.ts
+++ frontend/coffee-scraper/src/app/home/data-access/table.service.ts
@@ -1,16 +1,14 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { Product } from 'src/app/shared/models/product.model';
-import { HomeService } from './home.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TableService {
-  test: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  constructor(private homeService: HomeService) { }
+  doComparsion: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  constructor() { }
   
   doCompare() {
-    this.test.next(true);
+    this.doComparsion.next(true);
   }
 }
